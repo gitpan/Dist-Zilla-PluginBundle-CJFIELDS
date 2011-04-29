0ab8a06b7e74012162970e980ac9194fc1cca923
Christopher Fields <cjfields@bioperl.org>
2011-04-29 20:00:53 +0000

    Import of CJFIELDS/Dist-Zilla-PluginBundle-CJFIELDS-0.03 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-CJFIELDS
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CJFIELDS/Dist-Zilla-PluginBundle-CJFIELDS-0.03.tar.gz
    gitpan-cpan-author:       CJFIELDS
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/CJFIELDS.pm
+++ lib/Dist/Zilla/PluginBundle/CJFIELDS.pm
@@ -3,17 +3,31 @@ BEGIN {
   $Dist::Zilla::PluginBundle::CJFIELDS::AUTHORITY = 'cpan:CJFIELDS';
 }
 BEGIN {
-  $Dist::Zilla::PluginBundle::CJFIELDS::VERSION = '0.02';
+  $Dist::Zilla::PluginBundle::CJFIELDS::VERSION = '0.03';
 }
 
-# ABSTRACT: Build your modules like CJFIELDS
+# ABSTRACT: Build your modules like CJFIELDS (not sure that's a recommendation)
 
 use Moose 1.0;
 use Moose::Util::TypeConstraints;
-use MooseX::Types::Moose qw(Bool Str ArrayRef);
+use MooseX::Types::Moose qw(Bool Str HashRef Any);
 use namespace::autoclean;
 
 extends qw(Dist::Zilla::PluginBundle::FLORA);
+
+sub BUILD {
+    my $self = shift;
+    $self->_set_config_plugin( map { $_ => {} } qw(
+        MetaConfig
+        MetaJSON
+        PkgVersion
+        PodSyntaxTests
+        NoTabsTests
+        CompileTests
+        NextRelease
+        )
+    );
+}
 
 has '+authority'    => ( default     => 'cpan:CJFIELDS');
 
@@ -22,27 +36,92 @@ has '+github_user'  => ( default     => 'cjfields' );
 has 'create_readme' => (
     isa         => Bool,
     is          => 'ro',
-    default     => 1
+    default     => 1,
+    trigger     => sub {
+        my ($self, $val) = @_;
+        if (!$val) {
+            $self->_set_filter_plugin('Readme');
+        }
+        $val
+    }
 );
 
 has 'use_module_build'  => (
     isa         => Bool,
     is          => 'ro',
-    default     => 1
+    default     => sub {
+        my $self = shift;
+        $self->_set_filter_plugin('MakeMaker', 1);
+        $self->_set_config_plugin('ModuleBuild',{});
+        1;
+    },
+    trigger     => sub {
+        my ($self, $val) = @_;
+        if ($val) {
+            $self->_set_filter_plugin('MakeMaker',1);
+            $self->_set_config_plugin('ModuleBuild',{});
+        }
+        $val
+    }
 );
 
-has 'use_next_release'  => (
+has 'install_scripts'  => (
     isa         => Bool,
     is          => 'ro',
-    default     => 1
+    default     => 0
+);
+
+has 'scripts_dir'   =>(
+    isa         => 'Str',
+    is          => 'ro',
+    default     => 'scripts'
+);
+
+has 'skip_build'  => (
+    isa         => Bool,
+    is          => 'ro',
+    default     => 0,
+    lazy        => 1,
+    trigger     => sub {
+        my ($self, $val) = @_;
+        if ($val) {
+            Carp::croak() if $self->use_module_build;
+            $self->_set_filter_plugin('MakeMaker',1);
+        }
+        $val
+    }
+);
+
+has '_plugins_filtered' => (
+    traits      => ['Hash'],
+    isa         => HashRef[Bool],
+    is          => 'bare',
+    init_arg    => undef,
+    handles     => {
+        '_set_filter_plugin' => 'set',
+        '_filtered_plugins'  => 'keys'
+    }
+);
+
+has '_plugins_added' => (
+    traits      => ['Hash'],
+    isa         => HashRef[Any],
+    is          => 'bare',
+    init_arg    => undef,
+    handles     => {
+        '_set_config_plugin'     => 'set',
+        '_config_plugin_map'     => 'kv'
+    }
 );
 
 override 'configure' => sub {
     my $self = shift;
 
-    my @filtered;
-    push @filtered, 'Readme' if !$self->create_readme;
-    push @filtered, 'MakeMaker' if $self->use_module_build;
+    Carp::croak("Can't set both skip_build and use_module_build")
+        if $self->skip_build && $self->use_module_build;
+
+    # these need to be moved into a trigger or builder or somethin'
+    my @filtered = $self->_filtered_plugins;
 
     if (@filtered) {
         $self->add_bundle('@Filter' => {-bundle   => '@Basic',
@@ -51,16 +130,9 @@ override 'configure' => sub {
         $self->add_bundle('@Basic');
     }
 
-    $self->add_plugins(qw(
-                       MetaConfig
-                       MetaJSON
-                       PkgVersion
-                       PodSyntaxTests
-                       NoTabsTests
-                       CompileTests
-                       ))
-                       ;
+    $self->add_plugins($self->_config_plugin_map);
 
+    # these have to be set last-minute b/c they rely on secondary attributes
     $self->add_plugins(
         [MetaResources => {
             'repository.type'   => $self->repository_type,
@@ -75,17 +147,47 @@ override 'configure' => sub {
             do_metadata => 1,
         }],
         [EOLTests => {
-            trailing_whitespace => 1,
+            trailing_whitespace => !$self->disable_trailing_whitespace_tests,
         }],
     );
 
-    $self->add_plugins('ModuleBuild') if $self->use_module_build;
-    $self->add_plugins('PodCoverageTests') if !$self->disable_pod_coverage_tests;
-    $self->add_plugins('NextRelease') if $self->use_next_release;
+    $self->add_plugins(['ExecDir' => {
+            scripts => $self->scripts_dir
+        }]
+        ) if $self->install_scripts;
 
-    $self->add_plugins('AutoPrereqs') if $self->auto_prereqs;
+    $self->is_task
+        ? $self->add_plugins('TaskWeaver')
+        : $self->add_plugins(
+              [PodWeaver => {
+                  config_plugin => $self->weaver_config_plugin,
+              }],
+          );
 };
 
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::CJFIELDS - Build your modules like CJFIELDS (not sure that's a recommendation)
+
+=head1 AUTHOR
+
+Chris Fields <cjfields@bioperl.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Chris Fields.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
